5c7faf02dd5fab741c30cd35e6cb270a1facfab0
Mauro Nonis <mauro.nonis@gmail.com>
2021-10-26 10:48:42 +0200

    added github logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <v-app>
+    <v-app-bar color="transparent" absolute flat short>
+      <v-spacer />
+      <a href="https://github.com/BlazeRed/campo-smerdato">
+        <v-avatar size="32">
+          <v-img v-bind:src="require('@/assets/github-logo.png')" />
+        </v-avatar>
+      </a>
+    </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
